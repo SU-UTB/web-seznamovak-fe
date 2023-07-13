8b228d39d8569e3fe54103e33c301b92eafbec43
Filip Tomes <filiptomes@protonmail.com>
2023-07-13 10:40:49 +0200

    updated turnus fetching and data displaying

--- src/components/moleculs/TurnusItem.jsx
+++ src/components/moleculs/TurnusItem.jsx
@@ -1,68 +1,46 @@
 import { SpinnerCircular } from 'spinners-react'
-import { getAvailablePlaces } from '../../utils/getAvailablePlaces'
+import { Link } from 'react-router-dom'
+
 import TurnusHeadline from './TurnusHeadline'
-import { useEffect } from 'react';
-import api from '../../api/api';
 
 const TurnusItem = ({
   title,
   date,
-  range,
+  currentPlaces,
   totalPlaces,
   color,
-  data,
   isLoading,
   error,
   regLink,
   subRegLink,
-  outOfStock,
+  outOfStock = false,
 }) => {
-
-  // const {capacity, setCapacity} = useState();
-
-  useEffect(()=>{
-    api
-    .post(endpoint, formData)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error)
-    });
-  },[])
-
-  const availablePlaces = 20 // TODO: update according to real data
+  const available = currentPlaces > 0 && !outOfStock
 
   return (
     <div className="mx-4 mb-32 turnus lg:mb-12 xl:mb-0">
-      <TurnusHeadline title={title} date={date} range={range} color={color} />
+      <TurnusHeadline title={title} date={date} color={color} />
       {isLoading && (
         <div className="flex justify-center">
           <SpinnerCircular thickness={220} color="#FDECBE" />
         </div>
       )}
       {error && <p className="text-2xl text-beige">Neco se pokazilo...</p>}
-      {data.length > 0 && (
+      {currentPlaces && (
         <div>
           <p
             className="mb-8 text-3xl font-medium"
             style={{ color: `${color}` }}
           >
-            {availablePlaces > 0 && !outOfStock
-              ? `Zbývá ${availablePlaces}/${totalPlaces}`
-              : 'Vyprodáno'}
+            {available ? `Zbývá ${currentPlaces}/${totalPlaces}` : 'Vyprodáno'}
           </p>
-          <a
-            href={availablePlaces > 0 && !outOfStock ? regLink : subRegLink}
-            target="_blank"
-            rel="nopener noreferrer"
+          <Link
+            to={available ? regLink : subRegLink}
             className="block px-6 py-3 text-2xl font-bold text-beige"
             style={{ backgroundColor: `${color}` }}
           >
-            {availablePlaces > 0 && !outOfStock
-              ? 'PŘIHLÁSIT SE'
-              : 'PŘIHLÁSIT SE JAKO NÁHRADNÍK'}
-          </a>
+            {available ? 'PŘIHLÁSIT SE' : 'PŘIHLÁSIT SE JAKO NÁHRADNÍK'}
+          </Link>
         </div>
       )}
     </div>
--- src/components/sections/Application.jsx
+++ src/components/sections/Application.jsx
@@ -4,41 +4,14 @@ import Notification from '../moleculs/Notification'
 import TurnusHeadline from '../moleculs/TurnusHeadline'
 import TurnusItem from '../moleculs/TurnusItem'
 import Information from './Information'
+import useFetchReservations from '../../hooks/useFetchReservations'
 
 const Application = () => {
   // year:month(0-11):day:hour:minute:second
   const startDate = new Date(2021, 7, 14, 15, 0, 0)
   const shouldStartRegistration = Date.now() >= startDate
 
-  // TODO: get real data from database by an api call (useEffect)
-  const turnusList = [
-    {
-      title: '1. Turnus',
-      date: '21. 8. – 24. 8. 2023',
-      range: '(pondělí – čtvrtek)',
-      totalPlaces: 100,
-      color: '#E25525',
-      data: {},
-      isLoading: false,
-      error: 'Error',
-      regLink: 'https://forms.gle/qaEwv6kxMw4oHnQK6',
-      subRegLink: 'https://forms.gle/qaEwv6kxMw4oHnQK6',
-      outOfStock: true,
-    },
-    {
-      title: '2. Turnus',
-      date: '28. 8. – 31. 8. 2023',
-      range: '(pondělí – čtvrtek)',
-      totalPlaces: 100,
-      color: '#F3A548',
-      data: {},
-      isLoading: false,
-      error: 'Error',
-      regLink: 'https://forms.gle/F5k6XBFayw5TzZZFA',
-      subRegLink: 'https://forms.gle/F5k6XBFayw5TzZZFA',
-      outOfStock: true,
-    },
-  ]
+  const { data, isLoading, error } = useFetchReservations()
 
   return (
     <section id="prihlaska">
@@ -59,38 +32,32 @@ const Application = () => {
             </div>
           </div>
         )}
-        {shouldStartRegistration &&
-          turnusList.map(
-            ({
-              title,
-              date,
-              totalPlaces,
-              linkToAssign,
-              color,
-              data,
-              isLoading,
-              error,
-              regLink,
-              subRegLink,
-              outOfStock,
-            }) => (
-              <TurnusItem
-                key={title}
-                title={title}
-                date={date}
-                totalPlaces={totalPlaces}
-                linkToAssign={linkToAssign}
-                color={color}
-                data={data}
-                isLoading={isLoading}
-                error={error}
-                regLink={regLink}
-                subRegLink={subRegLink}
-                shouldStart={shouldStartRegistration}
-                outOfStock={outOfStock}
-              />
-            )
-          )}
+        {shouldStartRegistration && (
+          <>
+            <TurnusItem
+              title="1. Turnus"
+              date="21. 8. - 24. 8. 2023"
+              currentPlaces={data?.firstBatchCapacity}
+              totalPlaces={100}
+              color="#E25525"
+              isLoading={isLoading}
+              error={error}
+              regLink="/turnus1"
+              subRegLink=""
+            />
+            <TurnusItem
+              title="2. Turnus"
+              date="28. 8. - 31. 8. 2023"
+              currentPlaces={data?.secondBatchCapacity}
+              totalPlaces={100}
+              color="#F3A548"
+              isLoading={isLoading}
+              error={error}
+              regLink=""
+              subRegLink=""
+            />
+          </>
+        )}
       </div>
       <InfoText />
       <Information />
