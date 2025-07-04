d6421aff9455f8ae249a52c390d096c0de7db2a7
metalmasterboy <metalmasterboy@gmail.com>
2025-07-04 20:39:02 +0000

    Added main background

--- src/components/sections/Overlay.tsx
+++ src/components/sections/Overlay.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 //import '../../scss/Overlay.scss'
 import logo from '../../assets/img/logo.svg'
+import backgroundImage from '../../assets/img/main_bg.webp';
 
 const Overlay: React.FC = () => {
 
   return (
-    <div className="overlay relative pt-12 bg-primaryDark h-[35vh] md:h-[50vh] w-full m-0 overflow-y-hidden flex items-center justify-center">
-      {/* <div className="pattern"></div> */}
-      {/* <div className="pattern-mobile"></div> */}
-      {/* <div className="image"></div> */}
+    <div
+      className="overlay relative pt-12 bg-primaryDark h-[40vh] md:h-[70vh] w-full m-0 overflow-y-hidden flex items-center justify-center bg-cover md:bg-[center_top_60%]"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      {/* Overlay content goes here */}
+
       <img 
         src={logo} 
         alt="Logo" 
-        className="w-auto h-[25vh] md:h-[35vh]" // Adjust size as needed
+        className="w-auto h-[20vh] md:h-[35vh]" // Adjust size as needed
       />
     </div>
   )
