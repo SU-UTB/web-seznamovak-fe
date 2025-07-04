47498e062c842d379b636b69570cf40b5b4f3569
metalmasterboy <metalmasterboy@gmail.com>
2025-07-04 18:27:22 +0000

    Registration UTC check

--- src/components/sections/Application.tsx
+++ src/components/sections/Application.tsx
@@ -6,12 +6,15 @@ import TurnusItem from '../moleculs/TurnusItem'
 import Information from './Information'
 import useFetchReservations from '../../hooks/useFetchReservations'
 // Import the constants from your data/constants.ts file
-import { turnus1, turnus2, REGISTRATION_START_DATE } from '../../data/constants';
+import { turnus1, turnus2, REGISTRATION_START_DATE, REGISTRATION_END_DATE } from '../../data/constants';
 
 const Application = () => {
   // Use the REGISTRATION_START_DATE constant for the registration start time
-  const startDate = REGISTRATION_START_DATE.getTime();
-  const shouldStartRegistration = Date.now() >= startDate;
+  const nowUTC = Date.now();
+  const startDateUTC = Date.parse(REGISTRATION_START_DATE.toISOString());
+  const endDateUTC = Date.parse(REGISTRATION_END_DATE.toISOString());
+  
+  const isRegistrationOpen = nowUTC >= startDateUTC && nowUTC <= endDateUTC;
   // const shouldStartRegistration = true
 
   const { data, isLoading, error } = useFetchReservations();
@@ -19,7 +22,7 @@ const Application = () => {
   return (
     <section id="prihlaska" className='bg-primaryDark '>
       <div className="flex flex-wrap justify-around w-full pt-16 mx-auto lg:w-4/5 lg:pb-16">
-        {!shouldStartRegistration && (
+        {!isRegistrationOpen  && (
           <div className="w-full notify-headline">
             <Notification />
             <div className="flex flex-wrap justify-around w-full pt-16 mx-auto lg:w-4/5">
@@ -38,7 +41,7 @@ const Application = () => {
             </div>
           </div>
         )}
-        {shouldStartRegistration && (
+        {isRegistrationOpen  && (
           <>
             <TurnusItem
               outOfStock={false}
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -20,6 +20,7 @@ export interface Turnus {
   };
 
   export const REGISTRATION_START_DATE = new Date(2025, 6, 17, 15, 0, 0); // July 17, 2025, 15:00:00
+  export const REGISTRATION_END_DATE = new Date(2025, 7, 28, 15, 0, 0); // July 17, 2025, 15:00:00
   export const REGISTRATION_START_DATE_TEXT: string = "17. 7.";
   export const REGISTRATION_START_TIME_TEXT: string = "v 15:00";
 
