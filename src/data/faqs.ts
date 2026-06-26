e1b9c8aae4b14556bb7d1d12023f051d74187257
Pavos21 <metalmasterboy@gmail.com>
2026-06-26 13:42:31 +0200

    FB event link fix

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -20,7 +20,7 @@ export const faqs = [
   {
     question: 'JAK SE TAM DOSTANU?',
     answer:
-      'Na našem webu najdeš jak jízdní řád, tak mapu. Ze zastávky Bystřička, u Nováků se vydáš směrem jízdy, za hlukem. Není se kde ztratit! Přidej se do události na Facebooku pro <a style="text-decoration: underline;" href="https://fb.me/e/cn0anDI2e" target="_blank" rel="noreferrer">první</a> nebo <a style="text-decoration: underline;" href="https://fb.me/e/kxCYLrUF0" target="_blank" rel="noreferrer">druhý</a> turnus, kde najdeš info a brzy i parťáky, se kterými se můžeš domluvit na spolujízdu autem. Jestli budeš řídit Ty nebo se budeš chtít jen svézt, to už je na tobě, nezapomeň ale, že ten, kdo nedojel na seznamovák v kufru, jakoby nežil…',
+      'Na našem webu najdeš jak jízdní řád, tak mapu. Ze zastávky Bystřička, u Nováků se vydáš směrem jízdy, za hlukem. Není se kde ztratit! Přidej se do události na Facebooku pro <a style="text-decoration: underline;" href="https://www.facebook.com/events/1002924722436103" target="_blank" rel="noreferrer">první</a> nebo <a style="text-decoration: underline;" href="https://www.facebook.com/events/1561654445377816" target="_blank" rel="noreferrer">druhý</a> turnus, kde najdeš info a brzy i parťáky, se kterými se můžeš domluvit na spolujízdu autem. Jestli budeš řídit Ty nebo se budeš chtít jen svézt, to už je na tobě, nezapomeň ale, že ten, kdo nedojel na seznamovák v kufru, jakoby nežil…',
     showed: false,
   },
   {
